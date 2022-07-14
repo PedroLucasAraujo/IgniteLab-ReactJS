e92f2abb193c393581bfa73739348b1154bd839d
pedrolucas_araujo11 <pedrolucas.araujo@agenciam3.com>
2022-07-14 09:39:41 -0300

    fix: melhorando a organização do projeto

--- src/lib/apollo.ts
+++
@@ -1,6 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-export const client = new ApolloClient({
-  uri: "https://api-sa-east-1.hygraph.com/v2/cl4o8ddzj17y201xxggp8gobf/master",
-  cache: new InMemoryCache(),
-});
